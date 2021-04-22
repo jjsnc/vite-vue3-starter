a17f69242d42a5bd1574879acf62a0386286e00d
Damon <zhuowangyi@163.com>
2021-04-22 16:47:49 +0800

    build(deps): 单元测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,17 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended'
+  ],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {}
+  rules: { 'import/no-extraneous-dependencies': ['error', { devDependencies: true }] }
 }
